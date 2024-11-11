c434f3cc8111697b1aba86203c63381d3f8a95ae
Nava Wu <n36wu@uwaterloo.ca>
2024-11-11 17:16:45 -0500

    Added lab3 files + did kalman

--- Lab2/plot_errors.py
+++ Lab2/plot_errors.py
@@ -6,32 +6,41 @@ from utilities import FileReader
 
 def plot_errors(filename):
     
-    headers, values=FileReader(filename).read_file()
+    # Reading headers and values from the file
+    headers, values = FileReader(filename).read_file()
     
-    time_list=[]
-    
-    first_stamp=values[0][-1]
+    # Extracting the time values and normalizing to the first timestamp
+    time_list = []
+    first_stamp = values[0][-1]
     
     for val in values:
         time_list.append(val[-1] - first_stamp)
 
-    
-    
-    fig, axes = plt.subplots(1,2, figsize=(14,6))
+    # Creating subplots
+    fig, axes = plt.subplots(1, 2, figsize=(14, 6))
 
-
-    axes[0].plot([lin[0] for lin in values], [lin[1] for lin in values])
-    axes[0].set_title("state space")
-    axes[0].grid()
-
+    # State Space Plot
+    axes[0].plot([lin[0] for lin in values], 
+                [lin[1] for lin in values], marker='o')
+    axes[0].set_title("PID Controller: Linear Error vs. Linear Error Derivative")
+    axes[0].set_xlabel("Linear Error (m)")  # Label for x-axis
+    axes[0].set_ylabel("Linear Error Derivative (m/s)")  # Label for y-axis
+    axes[0].legend()
+    axes[0].grid(True)
     
-    axes[1].set_title("each individual state")
+    # Error State Behaviour Plot
+    axes[1].set_title("PD Controller (Parabola): Angular Error State Behaviour")
+    axes[1].set_xlabel("Time (s)")
+    axes[1].set_ylabel("Error Value")
+    
     for i in range(0, len(headers) - 1):
-        axes[1].plot(time_list, [lin[i] for lin in values], label= headers[i]+ " linear")
+        axes[1].plot(time_list, 
+                     [lin[i] for lin in values], 
+                     label=headers[i] + " angular", marker='o')
 
     axes[1].legend()
-    axes[1].grid()
-
+    axes[1].grid(True)
+    plt.tight_layout()
     plt.show()
     
     
