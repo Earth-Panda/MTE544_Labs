4b4c19bf15b9088c15adce3dbb8d65ce3a5e0f9d
Jason <j2heng@uwaterloo.ca>
2024-11-26 15:01:46 -0500

    fixed (for real this time)

--- Lab4/a_star.py
+++ Lab4/a_star.py
@@ -163,7 +163,7 @@ def search(maze, start, end):
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
             # TODO PART 4 Make sure within range (check if within maze boundary)
-            if(node_position[0] >= no_columns and node_position[0] < 0 and node_position[1] >= no_rows and node_position[1] < 0):
+            if(node_position[0] >= no_rows or node_position[0] < 0 or node_position[1] >= no_columns or node_position[1] < 0):
                 continue
 
             
